d05c065f0264892815b36614a627dc765bfc5e67
Андрей <anastasiiapegar@gmail.com>
2021-02-04 08:48:01 +0300

    Updates

---
+++ precache-manifest.3b59d0950104e99757a5155d0e106d11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42dcae26fa6929c5005c1f198febe53f",
+    "url": "/ReactReduxJSCardsProgect/index.html"
+  },
+  {
+    "revision": "d64473ce087820bc5efd",
+    "url": "/ReactReduxJSCardsProgect/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "8c9abd44e2ac24cf0ae7",
+    "url": "/ReactReduxJSCardsProgect/static/js/2.525a33b3.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/ReactReduxJSCardsProgect/static/js/2.525a33b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64473ce087820bc5efd",
+    "url": "/ReactReduxJSCardsProgect/static/js/main.6352a420.chunk.js"
+  },
+  {
+    "revision": "d48ffd80fa3231e9686d",
+    "url": "/ReactReduxJSCardsProgect/static/js/runtime-main.ce8cadf5.js"
+  }
+]);
